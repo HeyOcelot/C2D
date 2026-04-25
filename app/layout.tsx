40a0de658cbd9dfd8577b636d63f37a094f7cd3d
HeyOcelot <56937052+HeyOcelot@users.noreply.github.com>
2026-04-25 12:32:40 -0500

    Update metadata title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const nunito = Nunito({
 });
 
 export const metadata: Metadata = {
-  title: 'Nursery Dream',
-  description: 'A sanctuary for the world\'s newest arrivals.',
+  title: 'Come2Daddy',
+  description: '...And be your best little self!',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
